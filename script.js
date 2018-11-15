c12fba79ba3330f9cc4431eefe47b3faab1782e9
OdearOgy <hovhan198@gmail.com>
2018-11-15 23:35:06 +0400

    Fixed scrolling

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// defining variables 
+// defining variables
 const navBtn = document.getElementById('hamburger');
 const navBar = document.getElementById('nav-bar');
 
@@ -14,20 +14,6 @@ navBtn.addEventListener('click', event => {
       }
 });
 
-
-// scroll animation
-window.scroll({
-  top: 2500,
-  left: 0,
-  behavior: 'smooth'
-});
-
-// Scroll certain amounts from current position
-window.scrollBy({
-  top: 100, // could be negative value
-  left: 0,
-  behavior: 'smooth'
-});
 
 // Scroll to a certain element
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
